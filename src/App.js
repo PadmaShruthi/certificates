3fef3ad5ec64577656eb88621884398bf9b6e29a
Padma Shruthi <padma.shruthi30@gmail.com>
2021-06-30 21:04:48 +0530

    main commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,212 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import web3 from './web3';
+import ipfs from './ipfs';
+import storehash from './storehash';
+import { Button } from 'reactstrap';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class App extends Component {
+  state = {      
+    ipfsHash:null,      
+    buffer:'',      
+    ethAddress:'',      
+    transactionHash:'',      
+    txReceipt: ''    
+  };
+
+
+  //Take file input from user
+  captureFile =(event) => {event.stopPropagation() 
+    event.preventDefault() 
+    const file = event.target.files[0] 
+    let reader = new window.FileReader() 
+    reader.readAsArrayBuffer(file) 
+    reader.onloadend = () => this.convertToBuffer(reader) };
+
+
+  //Convert the file to buffer to store on IPFS 
+    convertToBuffer = async(reader) => {
+
+  //file is converted to a buffer for upload to IPFS        
+    const buffer = await Buffer.from(reader.result);   
+
+  //set this buffer-using es6 syntax        
+    this.setState({buffer});};
+
+  //ES6 async 
+    functiononClick = async () => {try{this.setState({blockNumber:"waiting.."});        
+    this.setState({gasUsed:"waiting..."});
+    await web3.eth.getTransactionReceipt(this.state.transactionHash, (err, txReceipt)=>{          
+      console.log(err,txReceipt);          
+      this.setState({txReceipt});        
+    });      
+  }
+
+  catch(error){      
+    console.log(error);   
+   }}
+
+
+   onSubmit = async (event) => {      
+     event.preventDefault();
+
+     //bring in user's metamask account address      
+     const accounts = await web3.eth.getAccounts();   
+
+     //obtain contract address from storehash.js      
+     const ethAddress= await storehash.options.address;      
+     this.setState({ethAddress});    
+
+     //save document to IPFS,return its hash#, and set hash# to state      
+     await ipfs.add(this.state.buffer, (err, ipfsHash) => {        
+       console.log(err,ipfsHash);        
+
+       //setState by setting ipfsHash to ipfsHash[0].hash        
+       this.setState({ ipfsHash:ipfsHash[0].hash });       
+       
+       
+       // call Ethereum contract method "sendHash" and .send IPFS hash to etheruem contract       
+      //return the transaction hash from the ethereum contract        
+      storehash.methods.sendhash1(this.state.ipfsHash).send({          
+        from: accounts[0]        
+      }, 
+        (error, transactionHash) => {          
+        console.log(transactionHash);          
+        this.setState({transactionHash});        
+      });  
+
+      storehash.methods.sendhash2(this.state.ipfsHash).send({          
+        from: accounts[0]        
+      }, 
+        (error, transactionHash) => {          
+        console.log(transactionHash);          
+        this.setState({transactionHash});        
+      });  
+
+      storehash.methods.sendhash3(this.state.ipfsHash).send({          
+        from: accounts[0]        
+      }, 
+        (error, transactionHash) => {          
+        console.log(transactionHash);          
+        this.setState({transactionHash});        
+      });  
+
+      storehash.methods.sendhash4(this.state.ipfsHash).send({          
+        from: accounts[0]        
+      }, 
+        (error, transactionHash) => {          
+        console.log(transactionHash);          
+        this.setState({transactionHash});        
+      });  
+
+      
+
+    })    
+  };
+
+
+  render() {
+    return (        
+    <div className="App">          
+    <header className="App-header">  
+    <h1>EduDecentro</h1>          
+    </header>
+
+    <hr/>
+    <grid>          
+      <h5> Choose Transcript file  </h5>          
+      <form onSubmit={this.onSubmit}>            
+      <input              
+      type = "file"              
+      onChange = {this.captureFile}            
+      />             
+      <Button             
+      bsStyle="primary"             
+      type="submit">             
+      Send it             
+      </Button> 
+      </form>
+      <tbody>                 
+         <tr>                   
+           <td>IPFS Hash</td>                   
+            <td> : </td>                    
+            <td>{this.state.ipfsHash}</td>                  
+            </tr>
+      </tbody>
+      
+
+      <h5> Choose Certificate-1 file  </h5>          
+      <form onSubmit={this.onSubmit}>            
+      <input              
+      type = "file"              
+      onChange = {this.captureFile}            
+      />             
+      <Button             
+      bsStyle="primary"             
+      type="submit">             
+      Send it             
+      </Button> 
+      </form>
+      <tbody>                 
+         <tr>                   
+           <td>IPFS Hash</td>                   
+            <td> : </td>                    
+            <td>{this.state.ipfsHash}</td>                  
+            </tr>
+      </tbody>
+
+      <h5> Choose Certificate-2 file  </h5>          
+      <form onSubmit={this.onSubmit}>            
+      <input              
+      type = "file"              
+      onChange = {this.captureFile}            
+      />             
+      <Button             
+      bsStyle="primary"             
+      type="submit">             
+      Send it             
+      </Button> 
+      </form>
+      <tbody>                 
+         <tr>                   
+           <td>IPFS Hash</td>                   
+            <td> : </td>                    
+            <td>{this.state.ipfsHash}</td>                  
+            </tr>
+      </tbody>
+
+      <h5> Choose Resume file  </h5>          
+      <form onSubmit={this.onSubmit}>            
+      <input              
+      type = "file"              
+      onChange = {this.captureFile}            
+      />             
+      <Button             
+      bsStyle="primary"             
+      type="submit">             
+      Send it             
+      </Button> 
+      </form>
+      <tbody>                 
+         <tr>                   
+           <td>IPFS Hash</td>                   
+            <td> : </td>                    
+            <td>{this.state.ipfsHash}</td>                  
+            </tr>
+      </tbody>
+
+
+       
+      <hr/> 
+      <Button             
+      bsStyle="primary"             
+      type="submit">             
+      LOGOUT            
+      </Button>
+      </grid>     
+    </div>      
+   );    
+}}
+
+ export default App;
---
+++ src/storehash.js
@@ -0,0 +1,109 @@
+import web3 from './web3';
+const address = '0xd9145CCE52D386f254917e481eB44e9943F39138';
+const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendhash1",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendhash2",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendhash3",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendhash4",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "gethash1",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "gethash2",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "gethash3",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "gethash4",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
